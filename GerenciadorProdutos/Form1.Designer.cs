5256505c68fc191334fa951142af569c3412ca4a
ANNE CAROLLINE EVARISTO CANGUSSÚ <anne.cangussu@e239.local>
2026-09-03 11:14:23 -0300

    eu sou anne e eu gosto de NARUTO

--- GerenciadorProdutos/Form1.Designer.cs
+++ GerenciadorProdutos/Form1.Designer.cs
@@ -28,75 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txt_nome = new System.Windows.Forms.TextBox();
-            this.txt_precoNome = new System.Windows.Forms.TextBox();
-            this.cmb_Tipo = new System.Windows.Forms.ComboBox();
-            this.txt_atributoEspecifico = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPrecoBase = new System.Windows.Forms.TextBox();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
             this.lbl_atributo = new System.Windows.Forms.Label();
             this.btn_salvar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblPrecoBase = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.txtAtributoEspecifico = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAtributo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // txt_nome
+            // txtNome
             // 
-            this.txt_nome.Location = new System.Drawing.Point(69, 94);
-            this.txt_nome.Name = "txt_nome";
-            this.txt_nome.Size = new System.Drawing.Size(100, 20);
-            this.txt_nome.TabIndex = 0;
+            this.txtNome.Location = new System.Drawing.Point(185, 75);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(170, 20);
+            this.txtNome.TabIndex = 0;
             // 
-            // txt_precoNome
+            // txtPrecoBase
             // 
-            this.txt_precoNome.Location = new System.Drawing.Point(326, 94);
-            this.txt_precoNome.Name = "txt_precoNome";
-            this.txt_precoNome.Size = new System.Drawing.Size(100, 20);
-            this.txt_precoNome.TabIndex = 1;
+            this.txtPrecoBase.Location = new System.Drawing.Point(185, 118);
+            this.txtPrecoBase.Name = "txtPrecoBase";
+            this.txtPrecoBase.Size = new System.Drawing.Size(170, 20);
+            this.txtPrecoBase.TabIndex = 1;
             // 
-            // cmb_Tipo
+            // cmbTipo
             // 
-            this.cmb_Tipo.FormattingEnabled = true;
-            this.cmb_Tipo.Items.AddRange(new object[] {
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
             "Físico",
             "Digital"});
-            this.cmb_Tipo.Location = new System.Drawing.Point(247, 208);
-            this.cmb_Tipo.Name = "cmb_Tipo";
-            this.cmb_Tipo.Size = new System.Drawing.Size(121, 21);
-            this.cmb_Tipo.TabIndex = 2;
-            // 
-            // txt_atributoEspecifico
-            // 
-            this.txt_atributoEspecifico.Location = new System.Drawing.Point(532, 94);
-            this.txt_atributoEspecifico.Name = "txt_atributoEspecifico";
-            this.txt_atributoEspecifico.Size = new System.Drawing.Size(100, 20);
-            this.txt_atributoEspecifico.TabIndex = 3;
+            this.cmbTipo.Location = new System.Drawing.Point(185, 166);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(170, 21);
+            this.cmbTipo.TabIndex = 2;
             // 
             // lbl_atributo
             // 
             this.lbl_atributo.AutoSize = true;
             this.lbl_atributo.Location = new System.Drawing.Point(400, 216);
             this.lbl_atributo.Name = "lbl_atributo";
-            this.lbl_atributo.Size = new System.Drawing.Size(35, 13);
+            this.lbl_atributo.Size = new System.Drawing.Size(0, 13);
             this.lbl_atributo.TabIndex = 4;
-            this.lbl_atributo.Text = "label1";
             // 
             // btn_salvar
             // 
-            this.btn_salvar.Location = new System.Drawing.Point(339, 256);
+            this.btn_salvar.Location = new System.Drawing.Point(335, 279);
             this.btn_salvar.Name = "btn_salvar";
             this.btn_salvar.Size = new System.Drawing.Size(75, 23);
             this.btn_salvar.TabIndex = 5;
             this.btn_salvar.Text = "Salvar";
             this.btn_salvar.UseVisualStyleBackColor = true;
             // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(98, 78);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 6;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblPrecoBase
+            // 
+            this.lblPrecoBase.AutoSize = true;
+            this.lblPrecoBase.Location = new System.Drawing.Point(98, 118);
+            this.lblPrecoBase.Name = "lblPrecoBase";
+            this.lblPrecoBase.Size = new System.Drawing.Size(65, 13);
+            this.lblPrecoBase.TabIndex = 7;
+            this.lblPrecoBase.Text = "Preço Base:";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(98, 166);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(31, 13);
+            this.lblTipo.TabIndex = 8;
+            this.lblTipo.Text = "Tipo:";
+            // 
+            // txtAtributoEspecifico
+            // 
+            this.txtAtributoEspecifico.Location = new System.Drawing.Point(185, 209);
+            this.txtAtributoEspecifico.Name = "txtAtributoEspecifico";
+            this.txtAtributoEspecifico.Size = new System.Drawing.Size(170, 20);
+            this.txtAtributoEspecifico.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Atributo:";
+            // 
+            // lblAtributo
+            // 
+            this.lblAtributo.AutoSize = true;
+            this.lblAtributo.Location = new System.Drawing.Point(377, 219);
+            this.lblAtributo.Name = "lblAtributo";
+            this.lblAtributo.Size = new System.Drawing.Size(16, 13);
+            this.lblAtributo.TabIndex = 11;
+            this.lblAtributo.Text = "...";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAtributo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAtributoEspecifico);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblPrecoBase);
+            this.Controls.Add(this.lblNome);
             this.Controls.Add(this.btn_salvar);
             this.Controls.Add(this.lbl_atributo);
-            this.Controls.Add(this.txt_atributoEspecifico);
-            this.Controls.Add(this.cmb_Tipo);
-            this.Controls.Add(this.txt_precoNome);
-            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.txtPrecoBase);
+            this.Controls.Add(this.txtNome);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -106,12 +160,17 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txt_nome;
-        private System.Windows.Forms.TextBox txt_precoNome;
-        private System.Windows.Forms.ComboBox cmb_Tipo;
-        private System.Windows.Forms.TextBox txt_atributoEspecifico;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPrecoBase;
+        private System.Windows.Forms.ComboBox cmbTipo;
         private System.Windows.Forms.Label lbl_atributo;
         private System.Windows.Forms.Button btn_salvar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblPrecoBase;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.TextBox txtAtributoEspecifico;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAtributo;
     }
 }
 
